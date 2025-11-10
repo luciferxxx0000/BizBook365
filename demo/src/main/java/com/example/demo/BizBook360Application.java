2ab8d7794cc3bc1cdd8bde999333399a5e9f274b
Dilshan <luciferxxx0000@gmail.com>
2025-11-11 01:10:32 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/demo/BizBook360Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizBook360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizBook360Application.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/demo/BizBook360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizBook360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
