7c3ededf7176a57e109c0cad73bf5dc5d93e7ac0
Ashen <ashen20060101@gmail.com>
2025-11-10 12:15:52 -0800

    Changes

--- demo/src/main/java/com/example/demo/adn/bbk/client/businesslayer/manager/CustomerBookingStatusControllerManager.java
+++ demo/src/main/java/com/example/demo/adn/bbk/client/businesslayer/manager/CustomerBookingStatusControllerManager.java
@@ -1,4 +1,5 @@
 package com.example.demo.adn.bbk.client.businesslayer.manager;
 
 public interface CustomerBookingStatusControllerManager {
+    void updateCustomerBookingStatus(Long bookingId, String status);
 }
